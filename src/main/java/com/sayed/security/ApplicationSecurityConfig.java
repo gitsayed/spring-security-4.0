5c458ff31183456f77717643e0e97a9e65249c35
gitsayed <abucollge@gmail.com>
2025-11-26 19:23:11 +0600

    commit 2

--- src/main/java/com/sayed/security/ApplicationSecurityConfig.java
+++ src/main/java/com/sayed/security/ApplicationSecurityConfig.java
@@ -14,14 +14,34 @@ public class ApplicationSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         http.authorizeHttpRequests(auth -> auth
-                .requestMatchers("/",
-                        "/index",
-                        "/css/*",
-                        "/js/*").permitAll()
-                .anyRequest().authenticated()
-        ).httpBasic(Customizer.withDefaults());
-
+                        .requestMatchers("/",
+                                "/index",
+                                "/css/*",
+                                "/js/*").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
+
+/*
+    // Default config written in the system
+    @Configurati on(
+            proxyBeanMethods = false
+    )
+    @ConditionalOnDefaultWebSecurity
+    static class SecurityFilterChainConfiguration {
+        @Bean
+        @Order(2147483642)
+        SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
+            http.authorizeHttpRequests((requests) -> ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated());
+            http.formLogin(Customizer.withDefaults());
+            http.httpBasic(Customizer.withDefaults());
+            return (SecurityFilterChain)http.build();
+        }
+    }
+
+ */
+
 
 }
